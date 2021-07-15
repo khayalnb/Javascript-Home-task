8980ad02379be7412a3395ac1c72dd2d58e42027
khayalnb <khayalnb@code.edu.az>
2021-07-15 21:31:15 +0400

    JavaScript home task

---
+++ assets/js/main.js
@@ -0,0 +1,75 @@
+const div1 =document.createElement('div')
+div1.style.width='10px'
+div1.style.height='300px'
+div1.style.backgroundColor='darkslategray'
+document.body.append(div1)
+
+
+const red = document.querySelector('#active-red')
+red.onclick = function(){
+
+}
+const yellow = document.querySelector('#active-yellow')
+yellow.onclick = function(){
+
+}
+const green = document.querySelector('#active-green')
+green.onclick = function(){
+
+}
+setInterval(function(){
+    red.style.backgroundColor = "#FF1E00",
+    red.style.boxShadow = "0px 0px 50px #FF1E00"
+    yellow.style.backgroundColor = "#ADB708",
+    yellow.style.boxShadow = "0px 0px 0px #ADB708",
+    green.style.backgroundColor= "#00FF00",
+    green.style.boxShadow = "0px 0px 0px #00FF00"
+ }, 5000)
+
+ setInterval(function(){
+    yellow.style.backgroundColor = "#FFE310",
+    yellow.style.boxShadow = "0px 0px 50px #FFFF20",
+    red.style.backgroundColor = "#B20000"
+    red.style.boxShadow = "0px 0px 0px #B20000"
+    green.style.backgroundColor= "#00FF00",
+    green.style.boxShadow = "0px 0px 0px #00FF00"
+ }, 7000)
+
+setInterval(function(){
+    green.style.backgroundColor = "#00FF1A",
+    green.style.boxShadow = "0px 0px 50px #00FF1A",
+    yellow.style.backgroundColor = "#ADB708",
+    yellow.style.boxShadow = "0px 0px 0px #ADB708"
+    red.style.backgroundColor = "#B20000"
+    red.style.boxShadow = "0px 0px 0px #B20000"
+},9000)
+
+setInterval(function(){
+    red.style.backgroundColor = "#FF1E00",
+    red.style.boxShadow = "0px 0px 50px #FF1E00",
+    yellow.style.backgroundColor = "#ADB708",
+    yellow.style.boxShadow = "0px 0px 0px #ADB708"
+    green.style.backgroundColor= "#00FF00",
+    green.style.boxShadow = "0px 0px 0px #00FF00"
+
+ }, 11000)
+
+ setInterval(function(){
+    yellow.style.backgroundColor = "#FFE310",
+    yellow.style.boxShadow = "0px 0px 50px #FFFF20",
+    green.style.backgroundColor= "#00FF00",
+    green.style.boxShadow = "0px 0px 0px #00FF00",
+    red.style.backgroundColor = "#B20000"
+    red.style.boxShadow = "0px 0px 0px #B20000"
+ }, 15000)
+
+ setInterval(function(){
+    green.style.backgroundColor = "#00FF1A",
+    green.style.boxShadow = "0px 0px 50px #00FF1A",
+    red.style.backgroundColor = "#B20000"
+    red.style.boxShadow = "0px 0px 0px #B20000"
+    yellow.style.backgroundColor = "#ADB708",
+    yellow.style.boxShadow = "0px 0px 0px #ADB708"
+ }, 17000)
+
+
