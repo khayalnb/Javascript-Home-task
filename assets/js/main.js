583d8e7734dbebc49337db6c85beed1fd6515925
khayalnb <khayalnb@code.edu.az>
2021-07-15 23:44:38 +0400

    function change

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,7 +17,7 @@ const green = document.querySelector('#active-green')
 green.onclick = function(){
 
 }
-setInterval(function(){
+setTimeout(function(){
     red.style.backgroundColor = "#FF1E00",
     red.style.boxShadow = "0px 0px 50px #FF1E00"
     yellow.style.backgroundColor = "#ADB708",
@@ -26,35 +26,35 @@ setInterval(function(){
     green.style.boxShadow = "0px 0px 0px #00FF00"
  }, 5000)
 
- setInterval(function(){
+ setTimeout(function(){
     yellow.style.backgroundColor = "#FFE310",
     yellow.style.boxShadow = "0px 0px 50px #FFFF20",
-    red.style.backgroundColor = "#B20000"
-    red.style.boxShadow = "0px 0px 0px #B20000"
+    red.style.backgroundColor = "#B20000",
+    red.style.boxShadow = "0px 0px 0px #B20000",
     green.style.backgroundColor= "#00FF00",
     green.style.boxShadow = "0px 0px 0px #00FF00"
  }, 7000)
 
-setInterval(function(){
+setTimeout(function(){
     green.style.backgroundColor = "#00FF1A",
     green.style.boxShadow = "0px 0px 50px #00FF1A",
     yellow.style.backgroundColor = "#ADB708",
-    yellow.style.boxShadow = "0px 0px 0px #ADB708"
-    red.style.backgroundColor = "#B20000"
+    yellow.style.boxShadow = "0px 0px 0px #ADB708",
+    red.style.backgroundColor = "#B20000",
     red.style.boxShadow = "0px 0px 0px #B20000"
 },9000)
 
-setInterval(function(){
+setTimeout(function(){
     red.style.backgroundColor = "#FF1E00",
     red.style.boxShadow = "0px 0px 50px #FF1E00",
     yellow.style.backgroundColor = "#ADB708",
-    yellow.style.boxShadow = "0px 0px 0px #ADB708"
+    yellow.style.boxShadow = "0px 0px 0px #ADB708",
     green.style.backgroundColor= "#00FF00",
     green.style.boxShadow = "0px 0px 0px #00FF00"
 
  }, 11000)
 
- setInterval(function(){
+ setTimeout(function(){
     yellow.style.backgroundColor = "#FFE310",
     yellow.style.boxShadow = "0px 0px 50px #FFFF20",
     green.style.backgroundColor= "#00FF00",
@@ -63,7 +63,7 @@ setInterval(function(){
     red.style.boxShadow = "0px 0px 0px #B20000"
  }, 15000)
 
- setInterval(function(){
+ set(function(){
     green.style.backgroundColor = "#00FF1A",
     green.style.boxShadow = "0px 0px 50px #00FF1A",
     red.style.backgroundColor = "#B20000"
